33da3aa7ff74be4bdca8fc526b66facac500ea19
Meghan Bock <meghan.bock@gmail.com>
2021-07-16 11:14:50 -0500

    setup inquier in index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,117 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { generateMarkdown } = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the name of your project?",
+      validate: (titleInput) => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log("Please enter the title of your project.");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please provide a description of your project.",
+      validate: (descriptionInput) => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log("Please enter a description for your project.");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "repoUrl",
+      message: "What is the URL for your repository?",
+      validate: (repoInput) => {
+        if (repoInput) {
+          return true;
+        } else {
+          console.log("Please enter the URL for your repository.");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "gitPagesUrl",
+      message: "What is the URL for your project's GitHub Page?",
+      validate: (gitPageInput) => {
+        if (gitPageInput) {
+          return true;
+        } else {
+          console.log("Please enter the URL for your's project's GitHub Page.");
+          return false;
+        }
+      },
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What type of license are you using for your project?",
+      choices: ["Slack", "Text", "Email", "Phone"], // to be updated
+    },
+  ]);
+};
+
+const promptReadMe = (readMeData) => {
+  if (!readMeData.projects) {
+    //readMeData.projects = [];
+  }
+  console.log(`
+        ========================
+        Create a new README file
+        ========================
+    `);
+    console.log(readMeData);
+    console.log(readMeData.title);
+};
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, readMeFile) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile("./dist/index.html", readMeFile, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve({
+        ok: true,
+        message: "File created!",
+      });
+    });
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  promptUser()
+    .then(promptReadMe)
+    .then(readMeData => {
+      return generateMarkdown(readMeData);
+    })
+    .then((readMeFile) => {
+      return writeFile(readMeFile);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,68 @@
+const licenseInfo = [
+  {
+    license: "x",
+    badge: "y",
+    link: "z",
+  },
+];
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  } else {
+    for (let i = 0; i < licenseInfo.length; i++) {
+      if (license == licenseInfo.license) {
+        return licenseInfo.badge;
+      } else {
+        return "";
+      }
+    }
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  } else {
+    for (let i = 0; i < licenseInfo.length; i++) {
+      if (license == licenseInfo.link) {
+        return licenseInfo.link;
+      } else {
+        return "";
+      }
+    }
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  } else {
+    return `
+  # ${license}
+  ## ${renderLicenseBadge(license)}
+  ## ${renderLicenseLink(license)}
+ `;
+  }
+};
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
+function generateMarkdown(readMeData) {
+  console.log(readMeData);
+  const { title, description, license, repoUrl, gitPagesUrl } = readMeData;
+  return `
+  # ${title}
+  \n-------------------\n
+  # ${description}
+  \n-------------------\n
+  ${renderLicenseSection(license)}
 `;
-}
+};
 
-module.exports = generateMarkdown;
+module.exports = { generateMarkdown };
